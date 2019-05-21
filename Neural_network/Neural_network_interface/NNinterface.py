9912c1294b8f165334620c18f685bf8737e9718c
Milena <milenamolska@gmail.com>
2019-05-21 14:05:59 +0200

    Added error to interface while data is incomplete

--- Neural_network/Neural_network_interface/NNinterface.py
+++ Neural_network/Neural_network_interface/NNinterface.py
@@ -19,25 +19,31 @@ class NNinterface(QMainWindow):
 		super().__init__()
 		uic.loadUi('graduation_prediction_interface.ui',self)
 		self.setWindowTitle('Graduation prediction app')
+		self.graduation.setVisible(False)
 	
 	def on_graduation_button_clicked(self):
-		gpa = float(self.gpa_edit.text())
-		maths_exam = float(self.maths_edit.text())
-		art_exam = float(self.art_edit.text())
-		language_exam = float(self.language_edit.text())
-		essay_score = float(self.essay_edit.text())
-		interview_score = float(self.interview_edit.text())
-		score = int(self.score_edit.text())
-		social_activity = self.social_box.value()
-	
-		probability = check_probability(gpa, maths_exam, art_exam, language_exam, social_activity, essay_score, interview_score, score)
-		self.graduation.setText(str(round(probability, 2))+'%')
-		if probability >= 70:
-			self.graduation.setStyleSheet("""QLineEdit {background-color: green}""")
-		elif probability >= 60:
-			self.graduation.setStyleSheet("""QLineEdit {background-color: yellow}""")
+		self.graduation.setVisible(True)
+		if self.gpa_edit.text() == "" or self.maths_edit.text() =="" or self.art_edit.text() == "" or self.language_edit.text() == "" or self. essay_edit.text() == "" or self.interview_edit.text() == "" or self.score_edit.text() == "":
+			self.graduation.setStyleSheet("""QLineEdit {background-color: white}""")
+			self.graduation.setText("!ERROR!")
 		else:
-			self.graduation.setStyleSheet("""QLineEdit {background-color: red}""")
+			gpa = float(self.gpa_edit.text())
+			maths_exam = float(self.maths_edit.text())
+			art_exam = float(self.art_edit.text())
+			language_exam = float(self.language_edit.text())
+			essay_score = float(self.essay_edit.text())
+			interview_score = float(self.interview_edit.text())
+			score = int(self.score_edit.text())
+			social_activity = self.social_box.value()
+		
+			probability = check_probability(gpa, maths_exam, art_exam, language_exam, social_activity, essay_score, interview_score, score)
+			self.graduation.setText(str(round(probability, 2))+'%')
+			if probability >= 70:
+				self.graduation.setStyleSheet("""QLineEdit {background-color: green}""")
+			elif probability >= 60:
+				self.graduation.setStyleSheet("""QLineEdit {background-color: yellow}""")
+			else:
+				self.graduation.setStyleSheet("""QLineEdit {background-color: red}""")
 		
 		
 		
